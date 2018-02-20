6039adf8dc24dd2871c326739f057c5381fe6b98
helenoktaviasiahaan <siahaanhelen13@gmail.com>
2018-02-20 19:54:34 +0700

    push pertama

---
+++ app/src/main/java/com/example/helen/helen_1202152325_modul2/DetailActivity.java
@@ -0,0 +1,13 @@
+package com.example.helen.helen_1202152325_modul2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail2);
+    }
+}
---
+++ app/src/main/java/com/example/helen/myapplication/HELEN_1202152325_MODUL2.java
@@ -0,0 +1,13 @@
+package com.example.helen.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HELEN_1202152325_MODUL2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_helen_1202152325__modul2);
+    }
+}
